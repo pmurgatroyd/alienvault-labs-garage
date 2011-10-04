40fb6889c354365e4020a228d186289b919617ce
jaime.blasco <jaime.blasco@alienvault.com>
2011-10-04 18:00:44 +0000

    Added some metasploit/libemu scripts

---
+++ metasploit/encoders.py
@@ -0,0 +1,48 @@
+import libemu
+import commands
+import os
+
+encoders = ["x64/xor", "x86/alpha_mixed", "x86/alpha_upper","x86/avoid_utf8_tolower","x86/call4_dword_xor","x86/context_cpuid","x86/context_stat","x86/context_time","x86/countdown","x86/fnstenv_mov","x86/jmp_call_additive","x86/nonalpha","x86/nonupper","x86/shikata_ga_nai", "x86/single_static_bit","x86/unicode_mixed","x86/unicode_upper"]
+
+PAYLOAD_EXE="/home/jaime/trunk/msfpayload"
+ENCODER_EXE="/home/jaime/trunk/msfencode"
+payload="windows/x64/meterpreter/bind_tcp"
+tmp_payload="/tmp/payload"
+
+results = open("results.csv","w")
+for e in encoders:
+	#./msfpayload windows/x64/meterpreter/bind_tcp R|./msfencode -e x64/xor -t raw -o tmp_payload
+	cmd = "%s %s R|%s -e %s -t raw -o %s" % (PAYLOAD_EXE, payload, ENCODER_EXE, e, tmp_payload)
+	os.system("rm /tmp/payload")
+	os.system(cmd)
+	try:
+		f = open(tmp_payload, "r")
+	except IOError:
+		continue
+	data = f.read()
+	f.close()
+	emulator = libemu.Emulator()
+	res = emulator.test(data)
+	print res
+	if res:
+		results.write("%s,%d\n" % (e, res))
+
+results.close()
+
+'''
+x64/xor,no detected
+x86/alpha_mixed,no detected
+x86/alpha_upper
+x86/avoid_utf8_tolower
+x86/call4_dword_xor,0
+x86/context_cpuid,0
+x86/context_stat,0
+x86/context_time,-4657153
+x86/countdown,0
+x86/fnstenv_mov,0
+x86/jmp_call_additive,1
+x86/nonalpha,-4657153
+x86/shikata_ga_nai,-4657153
+x86/single_static_bit,-4657153
+'''
+
---
+++ metasploit/payloads.py
@@ -0,0 +1,85 @@
+#./msfrpcd -P test -U test -S -t Web
+#http://dev.metasploit.com/redmine/projects/framework/wiki/XMLRPC
+
+import xmlrpclib
+import sys
+import time
+import os
+import stat
+import subprocess
+import threading
+import libemu
+import commands
+
+PAYLOAD_EXE="/home/jaime/trunk/msfpayload"
+
+class MsfBase:
+	def __init__(self):
+		self.proxy=""
+		self.token=""
+		self.console_id=""
+
+	def login(self,username,password):
+		try:
+			self.proxy = xmlrpclib.ServerProxy("http://localhost:55553")
+		
+			ret = self.proxy.auth.login(username,password)
+			if ret['result'] == 'success':
+				self.token = ret['token']
+				print self.token
+			else:
+				print "Could not login\n"
+				sys.exit(10)
+		except Exception:
+			print "Could not login: exception\n"
+			sys.exit(0)
+	
+	def getPayloads(self):
+		ret = self.proxy.module.payloads(self.token)
+		try:
+			return ret["modules"]
+		except:
+			return None
+
+	def getModuleOptions(self, type, name):
+		ret = self.proxy.module.options(self.token, type, name)
+		try:
+			return ret
+		except:
+			return None
+			
+
+conn = MsfBase()
+conn.login("test","test")
+
+f = open("detected.csv", "w+")
+
+payloads = conn.getPayloads()
+for p in payloads:
+	opts = {}
+	#print p
+	options = conn.getModuleOptions("payloads",p)
+	valid = True
+	for o in options.keys():
+		if options[o]["required"]:
+			if "default" not in options[o]:
+				#print "%s required" % o
+				if options[o] == "LHOST":
+					opts["LHOST"] = "192.168.1.1"
+				elif options[o] == "CMD":
+					opts["CMD"] = "ls"
+				else:
+					valid = False
+	if valid:
+		params = ""
+		for o in opts.keys():
+			params = params + "%s=%s " % (o, opts[o])
+		#print ("%s %s %s R" % (PAYLOAD_EXE, params, p))
+		data = commands.getstatusoutput("%s %s %s R" % (PAYLOAD_EXE, params, p))[1]
+		emulator = libemu.Emulator()
+		res = emulator.test(data)
+		if res:
+			f.write("%s,%d\n" % (p, res))
+
+f.close()
+
