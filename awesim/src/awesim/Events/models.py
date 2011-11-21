211298c971c0850f53f1d6303818dc55a610eea3
conrad <conrad@alienvault.com>
2011-11-21 16:10:29 +0000

    adding the Escalations app

--- awesim/src/awesim/Events/models.py
+++ awesim/src/awesim/Events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    name = models.CharField()
+    timestamp = models.DateTimeField()
+    # Event MetaData
+    
+    # Event Source Data
+    
+    # Event Hosts Data
+        # Sources
+        
+        # Destinations
+        
+    # Event Mapping Data
