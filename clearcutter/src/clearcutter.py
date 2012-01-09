b5df0cbb2097cf56c133fa7e6bbb4d9b579c5490
conrad <conrad@alienvault.com>
2012-01-09 22:35:01 +0000

    most of a working cli args parser with multimode now working.

--- clearcutter/src/clearcutter.py
+++ clearcutter/src/clearcutter.py
@@ -6,77 +6,53 @@ __author__ = "CP Constantine"
 __email__ = "conrad@alienvault.com"
 __copyright__ = 'Alienvault 2012'
 __credits__ = ["Conrad Constantine","Dominique Karg"]
-__version__ = "$Revision: 63990 $"
+__version__ = "0.1"
 __license__ = "GPL"
 __status__ = "Prototype"
 __maintainer__ = "CP Constantine"
 
-import sys,argparse,clusters #,ccregex
+import sys,argparse,clusters,ccregex
 
-log = ''
 
 class LogFile(object):
     '''
-    Contains the log file for other classes to reference to during processing
+    CLog File object with a few helper functions for other clearcutter modes
     '''    
     _filedata = ""
     Filename = ""
     
-    def __init__(self, filename):
-        self._filedata = open(filename,'r')
+    def __init__(self, filename, verbose=False):
         self.Filename = filename
-           
-    def RetrieveCurrentLine(self):
+        try:             
+            if verbose == True : print "Using File: " + filename
+            self._filedata = open(filename,'r')
+        except ValueError:
+            if verbose == True : print "Invalid Filename: " + sys.exc_info()[2]
+            raise sys.exc_info()
+        except IOError:
+            if verbose == True : print "File Access Error: " + sys.exc_info()[2]
+            raise sys.exc_info()
+        
+    def RetrieveCurrentLine(self, verbose=False ):
+        if verbose == True : print "Reading Line " + self._filedata.tell()
         return self._filedata.readline()
     
-
-    #try:
-    #    log = LogFile(sys.argv[2])
-    #    print "Using Log File %s" % sys.argv[2]
-    #except IndexError:
-    #    print "no "
-    #except ValueError:
-    #    print "File load error" 
-    #    print sys.exc_info()[0]
-
-
-
-        #print "Log Extraction Mode (identify unique log messages)"
-        #print "\n\t%s extract <sample file> [threshold] [outfile]"  % sys.argv[0]
-        #print "\n\t\t<sample file> - log sample file to process"
-        #print "\n\t\t[threshold] - how many values a field should have to be considered variable"
-        #print "\n\t\t[outfile] - file to write potential unique log messages to"
-        #<toggle common regexps
         
-        #note to user that large files take a very long time
         # build a spinner?
-        
-        #print "Plugin Parsing Mode"
-        #    parsing
-        #        matching
-        #            Summary
-        #            Display Matched
-        #            Display Unmatched
-        #            Display Matched Offset
-        #        profiling
-        #            SID profiling
-        #            Log profiling
-        #            SID re-ordering
-        
-
-        
+                
         
 def DoLogExtract(args):
     """Commence Log Message Extraction mode""" 
     try:
-        log = LogFile(args.filename)
+        log = LogFile(args.logfile)
     except IOError:
-        print "File: " + filename + " cannot be opened : " + str(sys.exc_info()[1])
+        print "File: " + log.Filename + " cannot be opened : " + str(sys.exc_info()[1])
         sys.exit()
       
     myclusters = clusters.ClusterGroup()
     logline = log.RetrieveCurrentLine() 
     while logline != "":
+        #update progress bar
         myclusters.IsMatch(logline)
         logline = log.RetrieveCurrentLine()
         
@@ -85,9 +61,9 @@ def DoLogExtract(args):
 def DoLogParse(args):
     """Commence Plugin Parsing Mode"""
     try:
-        log = LogFile(args.filename)
+        log = LogFile(args.logfile)
     except IOError:
-        print "File: " + filename + " cannot be opened : " + str(sys.exc_info()[1])
+        print "File: " + log.Filename + " cannot be opened : " + str(sys.exc_info()[1])
         sys.exit()
     myregexps = ccregex.ParsePlugin(args)
     myregexps.Parse()
@@ -107,40 +83,47 @@ def DoLogProfile():
 
 
 #=========================
-mode = {'extract' : DoLogExtract, 'parse' : DoLogParse, 'profile' : DoLogProfile}
+mode = {'identify' : DoLogExtract, 'parse' : DoLogParse, 'profile' : DoLogProfile}
         
 def ParseArgs():
     parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter, \
                                      description='Processes log files for SIEM consumption',
                                      epilog='examples:\n\t%(prog)s identify sample.log\n\t%(prog)s parse plugin.cfg sample.log\n\t%(prog)s profile plugin.cfg sample.log')
     modeparsers = parser.add_subparsers(help='Command Mode Help')
-        
-    parser.add_argument('-v', action='count',help='verbose mode - use multiple times to increase verbosity')
-    parser.add_argument('-q', action='store_true',help='quiet mode - print nothing but results')
-    parser.add_argument('-o', action='store', type = str, metavar = 'file' , help='Write results to <file>')
+           
+    identifyparser = modeparsers.add_parser('identify',help='Log Event Candidate Identification')
+    identifyparser.set_defaults(mode='identify') 
+    identifyparser.add_argument(dest='logfile', action='store', type=str, help='log file to process')
+    identifyparser.add_argument('-t', action='store', type=int, help='Threshold value for Variable Assignment', )
+    identifyparser.add_argument('-v', action='count',help='verbose mode - use multiple times to increase verbosity')
+    identifyparser.add_argument('-q', action='store_true',help='quiet mode - print nothing but results')
+    identifyparser.add_argument('-o', action='store', type = str, metavar = 'file' , help='Write results to <file>')
     
-    logextractparser = modeparsers.add_parser('identify',help='Log Event Candidate Identification')
-    logextractparser.add_argument('extract - Log Event Message Type Extraction')
-    logextractparser.add_argument('filename', action='store',help='log file to process')
-    logextractparser.add_argument('-t', '--threshold', type=int, action='store', help='Threshold value for Variable Assignment', )
     
-    logparseparser = modeparsers.add_parser('parse',help = 'OSSIM Plugin Parse Testing')
-    logparseparser.add_argument('plugin', action='store',help='OSSIM plugin .cfg file')
-    logparseparser.add_argument('-n', action='store',help='Show Matching values from position N')    
+    pluginparser = modeparsers.add_parser('parse',help = 'OSSIM Plugin Parse Testing')
+    pluginparser.set_defaults(mode='parse')
+    pluginparser.add_argument(dest='plugin', action='store', type=str,help='OSSIM plugin .cfg file')
+    pluginparser.add_argument('-n', action='store', type=int, help='Show Matching values from position N')    
+    pluginparser.add_argument('-v', action='count',help='verbose mode - use multiple times to increase verbosity')
+    pluginparser.add_argument('-q', action='store_true',help='quiet mode - print nothing but results')
+    pluginparser.add_argument('-o', action='store', type = str, metavar = 'file' , help='Write results to <file>')
+    
     
     profileparser = modeparsers.add_parser('profile',help = 'OSSIM Plugin Performance Profiling')
-    profileparser.add_argument('plugin', action='store',help='OSSIM plugin .cfg file')
+    profileparser.set_defaults(mode='profile')
+    profileparser.add_argument('plugin', action='store', type=str,help='OSSIM plugin .cfg file')
     profileparser.add_argument('-s','--sort', action='store_true',help='Sort Results by Execution Time')
+    profileparser.add_argument('-v', action='count',help='verbose mode - use multiple times to increase verbosity')
+    profileparser.add_argument('-q', action='store_true',help='quiet mode - print nothing but results')
+    profileparser.add_argument('-o', action='store', type = str, metavar = 'file' , help='Write results to <file>')
     
-    #profilegroup.add_argument()
-    #profilegroup.add_argument()
+    globalargs = parser.parse_args()
+    print globalargs
     
-    args = parser.parse_args()
-    
-    if args.mode not in mode.iterkeys():
+    if globalargs.mode not in mode.iterkeys():
         parser.print_usage(None)
     else:
-        mode[args.mode](args)
+        mode[globalargs.mode](globalargs)
 
 if __name__ == '__main__':
     ParseArgs()
