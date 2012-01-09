7ed79f4a5bc57d2fc7f831f9e090d3ec26e8852a
conrad <conrad@alienvault.com>
2012-01-09 18:43:23 +0000

    adding the full options to argparse stuff

--- clearcutter/src/clearcutter.py
+++ clearcutter/src/clearcutter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import clusters,sys,argparse
+import sys,argparse,clusters #,ccregex
 
 log = ''
 
@@ -16,9 +16,6 @@ class LogFile(object):
            
     def RetrieveCurrentLine(self):
         return self._filedata.readline()
-    
-        
-
     
 
     #try:
@@ -59,7 +56,12 @@ class LogFile(object):
         
 def DoLogExtract(args):
     """Commence Log Message Extraction mode""" 
-    log = LogFile(args.filename)
+    try:
+        log = LogFile(args.filename)
+    except IOError:
+        print "File: " + filename + " cannot be opened : " + str(sys.exc_info()[1])
+        sys.exit()
+      
     myclusters = clusters.ClusterGroup()
     logline = log.RetrieveCurrentLine() 
     while logline != "":
@@ -67,14 +69,16 @@ def DoLogExtract(args):
         logline = log.RetrieveCurrentLine()
         
     myclusters.Results()
-    # import clusters
-    # process clusters
     
-    
-    
-def DoLogParse():
+def DoLogParse(args):
     """Commence Plugin Parsing Mode"""
-    
+    try:
+        log = LogFile(args.filename)
+    except IOError:
+        print "File: " + filename + " cannot be opened : " + str(sys.exc_info()[1])
+        sys.exit()
+    myregexps = ccregex.ParsePlugin(args)
+    myregexps.Parse()
     #import logregex
     #process log from plugin
     pass
@@ -94,13 +98,22 @@ def ParseArgs():
     parser = argparse.ArgumentParser(description='Processes log files for SIEM consumption')
         
     parser.add_argument('mode', action='store',choices=['extract','parse','profile'], help='extract (find potential unique log event types) ------ parse (parse a log file using an OSSIM plugin config)\nprofile (generate performance data) ')
-    parser.add_argument('filename', action='store',help='log file to process')
-    
+    parser.add_argument('-v', type=int, action='store',help='verbose mode')
+        
     extractgroup = parser.add_argument_group('Log Event Message Type Extraction')
+    extractgroup.add_argument('filename', action='store',help='log file to process')
+    extractgroup.add_argument('threshold', type=int, action='store',help='log file to process')
     
+       
     parsegroup = parser.add_argument_group('OSSIM Plugin Parse Testing')
+    parsegroup.add_argument('plugin', action='store',help='OSSIM plugin .cfg file')
+    #parsegroup.add_argument()
+    #parsegroup.add_argument()
     
     profilegroup = parser.add_argument_group('OSSIM Plugin Performance Profiling')
+    profilegroup.add_argument('plugin', action='store',help='OSSIM plugin .cfg file')
+    #profilegroup.add_argument()
+    #profilegroup.add_argument()
     
     args = parser.parse_args()
     
@@ -112,4 +125,3 @@ def ParseArgs():
 if __name__ == '__main__':
     ParseArgs()
 
-#==========================
